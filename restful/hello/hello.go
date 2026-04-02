5f89edfe0666ac20385e2e77ddd9cd5f0262acbb
王宇瞮 <459440374@qq.com>
2026-04-02 23:47:24 +0800

    feat: 暂时删除demo

--- restful/hello/hello.go
+++ restful/hello/hello.go
@@ -4,8 +4,10 @@
 package main
 
 import (
+	"embed"
 	"flag"
 	"fmt"
+	"net/http"
 
 	"Quantum/restful/hello/internal/config"
 	"Quantum/restful/hello/internal/handler"
@@ -15,7 +17,10 @@ import (
 	"github.com/zeromicro/go-zero/rest"
 )
 
-var configFile = flag.String("f", "etc/hello.yaml", "the config file")
+//go:embed assets/index.html
+var content embed.FS
+
+var configFile = flag.String("f", "etc/hello-dev.yaml", "the config file")
 
 func main() {
 	flag.Parse()
@@ -28,6 +33,17 @@ func main() {
 
 	ctx := svc.NewServiceContext(c)
 	handler.RegisterHandlers(server, ctx)
+
+	server.AddRoute(rest.Route{
+		Method: http.MethodGet,
+		Path:   "/",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// 从 embed 中读取并返回 index.html
+			file, _ := content.ReadFile("assets/index.html")
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			w.Write(file)
+		}),
+	})
 
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
 	server.Start()
